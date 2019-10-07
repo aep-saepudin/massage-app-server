52682a4270489dd6362fdfc08f16ec741488caba
juara <aep.stmik@gmail.com>
2019-10-08 01:23:46 +0700

    Fix config dan product

--- massage-app-server/apis/config.php
+++ massage-app-server/apis/config.php
@@ -1,8 +1,8 @@
 <?php 
     $servername  = "localhost";
-    $db_username = "root";
-    $db_password = "";
-    $dbname      = "blind_app";
+    $db_username = "blindmas_app";
+    $db_password = ";%5^)2,o;X,9";
+    $dbname      = "blindmas_blind_app";
 
     $BASE_URL     = 'https://firestore.googleapis.com/';
     $DATABASE_URL = '/v1/projects/massage-blind/databases/%28default%29/';
